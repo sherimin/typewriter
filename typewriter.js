b066d5cf85043235f3532f6df77a04c242e28e0d
Sheri Lu <sherisherilu@gmail.com>
2022-10-11 21:57:10 +0000

    initial commit - LHL M1 Animated Sentence

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+const sentence = "hello there from lighthouse labs";
+
+
+for (let i = 0; i <= sentence.length; i++) {
+    setTimeout(() => {
+        if (i === sentence.length) {
+            console.log('\n');
+        } else {
+            process.stdout.write(sentence[i]);
+        }
+    },i * 50)
+  }
+
+
+
+
+
+
